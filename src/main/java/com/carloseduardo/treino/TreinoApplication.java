294ad12b2b308018af7f3864305972664fdfc5bf
CarlosEDsl <carloseduardoestds@gmail.com>
2024-04-22 16:06:05 -0300

    Spring Boot Project

---
+++ src/main/java/com/carloseduardo/treino/TreinoApplication.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.treino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoApplication.class, args);
+	}
+
+}
