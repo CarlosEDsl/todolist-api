54054d337f1341d8e2e3569ec0b6b55099dd84c3
CarlosEDsl <carloseduardoestds@gmail.com>
2024-04-25 16:15:46 -0300

    UserService creation

---
+++ src/main/java/com/carloseduardo/treino/services/UserService.java
@@ -0,0 +1,52 @@
+package com.carloseduardo.treino.services;
+
+import com.carloseduardo.treino.models.User;
+import com.carloseduardo.treino.repositories.TaskRepository;
+import com.carloseduardo.treino.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(()-> new RuntimeException(
+                "Usuário não encontrado no ID: " + id + "Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null); // Garantir que não será enviado um usuário com ID
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try{
+            this.userRepository.deleteById(id);
+
+        }catch (Exception e) {
+            throw new RuntimeException("O usuário ainda possuí relacionamentos, logo não pode ser deletado");
+        }
+    }
+
+}
