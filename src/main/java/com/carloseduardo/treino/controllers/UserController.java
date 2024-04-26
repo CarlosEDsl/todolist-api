04fba03a47fa55f611db385ecec0e885e54e78a6
CarlosEDsl <carloseduardoestds@gmail.com>
2024-04-26 17:58:12 -0300

    User Refactoration

---
+++ src/main/java/com/carloseduardo/treino/controllers/UserController.java
@@ -0,0 +1,2 @@
+package com.carloseduardo.treino.controllers;public class UserController {
+}
--- src/main/java/com/carloseduardo/treino/models/User.java
+++ src/main/java/com/carloseduardo/treino/models/User.java
@@ -1,5 +1,6 @@
 package com.carloseduardo.treino.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -76,6 +77,7 @@ public class User {
         this.password = password;
     }
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return tasks;
     }
