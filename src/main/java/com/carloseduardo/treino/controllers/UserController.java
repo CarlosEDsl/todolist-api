63126a4e45248a5361b078df8d6009b3f857884b
CarlosEDsl <carloseduardoestds@gmail.com>
2024-04-26 17:58:42 -0300

    UserController creation

--- src/main/java/com/carloseduardo/treino/controllers/UserController.java
+++ src/main/java/com/carloseduardo/treino/controllers/UserController.java
@@ -1,2 +1,50 @@
-package com.carloseduardo.treino.controllers;public class UserController {
+package com.carloseduardo.treino.controllers;
+
+import com.carloseduardo.treino.models.User;
+import com.carloseduardo.treino.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = this.userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    @Validated(User.CreateUser.class)
+    public ResponseEntity<Void> create(@Valid @RequestBody User obj){
+        this.userService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    @Validated(User.UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody User obj, @PathVariable Long id){
+        obj.setId(id);
+        this.userService.update((obj));
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
