9f21c5ad3e44c5eee04e1c85519de8c209662565
CarlosEDsl <carloseduardoestds@gmail.com>
2024-04-30 18:18:12 -0300

    GlobalExceptionHandler Creation

---
+++ src/main/java/com/carloseduardo/treino/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.treino.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+
+}
