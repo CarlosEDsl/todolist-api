8fddf8be264b9417999b81df3c1763ff882bedec
CarlosEDsl <carloseduardoestds@gmail.com>
2024-05-02 15:41:18 -0300

    Refactoring and creating exception handlers

--- src/main/java/com/carloseduardo/treino/exceptions/ErrorResponse.java
+++ src/main/java/com/carloseduardo/treino/exceptions/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.carloseduardo.treino.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.Objects;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private final int status;
     private final String message;
