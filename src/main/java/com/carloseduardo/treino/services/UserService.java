e855ff37a0714f6e8947d49d2b687871e0398b89
CarlosEDsl <carloseduardoestds@gmail.com>
2024-05-07 15:26:13 -0300

    UserService security implementation

--- src/main/java/com/carloseduardo/treino/services/UserService.java
+++ src/main/java/com/carloseduardo/treino/services/UserService.java
@@ -1,17 +1,24 @@
 package com.carloseduardo.treino.services;
 
 import com.carloseduardo.treino.models.User;
+import com.carloseduardo.treino.models.enums.ProfileEnum;
 import com.carloseduardo.treino.repositories.UserRepository;
 import com.carloseduardo.treino.services.exceptions.DataBindingViolationException;
 import com.carloseduardo.treino.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -26,6 +33,8 @@ public class UserService {
     @Transactional
     public User create(User obj) {
         obj.setId(null); // Garantir que não será enviado um usuário com ID
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -34,6 +43,7 @@ public class UserService {
     public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
