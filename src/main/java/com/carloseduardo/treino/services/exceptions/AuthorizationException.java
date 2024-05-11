8d730328ac363392dc181b301677d0650ea3ba71
CarlosEDsl <carloseduardoestds@gmail.com>
2024-05-11 12:58:57 -0300

    AuthorizationException Creation

--- src/main/java/com/carloseduardo/treino/services/exceptions/AuthorizationException.java
+++ src/main/java/com/carloseduardo/treino/services/exceptions/AuthorizationException.java
@@ -1,2 +1,15 @@
-package com.carloseduardo.treino.services.exceptions;public class AuthorizationException {
+package com.carloseduardo.treino.services.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class AuthorizationException extends AccessDeniedException {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
 }
